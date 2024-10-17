ee78debceaf322f1b31005a417510dbe30d90cb0
Jack C <85141287+jackc6243@users.noreply.github.com>
2024-10-17 15:26:25 +1100

    .

--- database.py
+++ database.py
@@ -2,38 +2,51 @@
 import psycopg2
 
 #####################################################
-##  Database Connection
+# Database Connection
 #####################################################
 
 '''
 Connect to the database using the connection string
 '''
+
+
 def openConnection():
     # connection parameters - ENTER YOUR LOGIN AND PASSWORD HERE
     userid = "y24s2c9120_unikey"
     passwd = ""
     myHost = "awsprddbs4836.shared.sydney.edu.au"
 
-
     # Create a connection to the database
     conn = None
     try:
         # Parses the config file and connects using the connect string
         conn = psycopg2.connect(database=userid,
-                                    user=userid,
-                                    password=passwd,
-                                    host=myHost)
+                                user=userid,
+                                password=passwd,
+                                host=myHost)
 
     except psycopg2.Error as sqle:
         print("psycopg2.Error : " + sqle.pgerror)
-    
+
     # return the connection to use
     return conn
+
 
 '''
 Validate staff based on username and password
 '''
+
+
 def checkLogin(login, password):
+    try:
+        conn = openConnection()
+        curs = conn.cursor()
+
+    except psycopg2.Error as sqle:
+        return None
+    finally:
+        curs.close()
+        conn.close()
 
     return ['jdoe', 'John', 'Doe', 'jdoe@csh.com']
 
@@ -41,6 +54,8 @@ def checkLogin(login, password):
 '''
 List all the associated admissions records in the database by staff
 '''
+
+
 def findAdmissionsByAdmin(login):
 
     return
@@ -50,6 +65,8 @@ def findAdmissionsByAdmin(login):
 Find a list of admissions based on the searchString provided as parameter
 See assignment description for search specification
 '''
+
+
 def findAdmissionsByCriteria(searchString):
 
     return
@@ -58,15 +75,18 @@ def findAdmissionsByCriteria(searchString):
 '''
 Add a new addmission 
 '''
+
+
 def addAdmission(type, department, patient, condition, admin):
-    
+
     return
 
 
 '''
 Update an existing admission
 '''
+
+
 def updateAdmission(id, type, department, dischargeDate, fee, patient, condition):
-    
 
     return
